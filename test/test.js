09b7a9ff2cd8623ef5445440d9c48df986a4ca7a
Brandon Aaron <brandon.aaron@gmail.com>
2013-02-08 13:17:25 -0600

    remove console.log statement

--- test/test.js
+++ test/test.js
@@ -4,9 +4,8 @@ function testWithVersion(version/*args*/) {
   if (isVersionGreater($.fn.jquery, version)) {
     test.apply(window, args);
   }
-  
+
   function isVersionGreater(v1, v2) {
-    console.log(v1, v2);
     var v1 = parseInt(v1.split('.').join(''), 10),
         v2 = parseInt(v2.split('.').join(''), 10);
     return (v1 >= v2);
@@ -16,19 +15,19 @@ function testWithVersion(version/*args*/) {
 // should work with all versions of jQuery
 testWithVersion('1.0.4', "copyEvents()", function() {
     var clickCount = mouseoverCount = mouseoutCount = 0;
-    
+
     // Create event handlers for A tag
     $('#main a.from')
-        .bind('click', function() { 
-            clickCount++; 
+        .bind('click', function() {
+            clickCount++;
         })
         .bind('mouseover', function() {
             mouseoverCount++;
         })
         .bind('mouseout', function() {
             mouseoutCount++;
-        }); 
-        
+        });
+
     // clone events from a to 'from' li tags
     $('#main li.from')
         .copyEvents('#main a.from')
@@ -36,12 +35,12 @@ testWithVersion('1.0.4', "copyEvents()", function() {
         .trigger('click')
         .trigger('mouseover')
         .trigger('mouseout');
-    
+
     // check counts
     ok( clickCount == 2, 'Testing click count' );
     ok( mouseoverCount == 2, 'Testing mouseover count' );
     ok( mouseoutCount == 2, 'Testing mouseout count' );
-    
+
     // just making sure there are no errors when copying to or from an empty set or from elements with no events
     $('#does_not_exist').copyEvents('#also_does_not_exist');
     $('<div />').copyEvents('#does_not_exist');
@@ -50,29 +49,29 @@ testWithVersion('1.0.4', "copyEvents()", function() {
 // should work with all versions of jQuery
 testWithVersion('1.0.4', "copyEventsTo()", function() {
     var clickCount = mouseoverCount = mouseoutCount = 0;
-    
+
     // Create event handlers for A tag
     $('#main a.to')
-        .bind('click', function() { 
-            clickCount++; 
+        .bind('click', function() {
+            clickCount++;
         })
         .bind('mouseover', function() {
             mouseoverCount++;
         })
         .bind('mouseout', function() {
             mouseoutCount++;
-        }); 
-    
+        });
+
     // clone events from a to 'from' li tags
     $('#main a.to')
         .copyEventsTo('#main li.to');
-        
+
     $('#main li.to')
         // fire events
         .trigger('click')
         .trigger('mouseover')
         .trigger('mouseout');
-    
+
     // check counts
     ok( clickCount == 2, 'Testing click count' );
     ok( mouseoverCount == 2, 'Testing mouseover count' );
@@ -82,12 +81,12 @@ testWithVersion('1.0.4', "copyEventsTo()", function() {
 // should work since 1.1.x when data was introduced
 testWithVersion('1.1.4', "copyEvents() with data", function() {
     var clickCount = mouseoverCount = mouseoutCount = dataCount = 0;
-    
+
     // Create event handlers for A tag
     $('#main a.from')
-        .bind('click', { test: true }, function(event) { 
+        .bind('click', { test: true }, function(event) {
             if (event.data.test) dataCount++;
-            clickCount++; 
+            clickCount++;
         })
         .bind('mouseover', { test: true }, function(event) {
             if (event.data.test) dataCount++;
@@ -97,7 +96,7 @@ testWithVersion('1.1.4', "copyEvents() with data", function() {
             if (event.data.test) dataCount++;
             mouseoutCount++;
         });
-        
+
     // clone events from a to 'from' li tags
     $('#main li.from')
         .copyEvents('#main a.from')
@@ -105,7 +104,7 @@ testWithVersion('1.1.4', "copyEvents() with data", function() {
         .trigger('click')
         .trigger('mouseover')
         .trigger('mouseout');
-    
+
     // check counts
     ok( clickCount == 2, 'Testing click count' );
     ok( mouseoverCount == 2, 'Testing mouseover count' );
@@ -116,12 +115,12 @@ testWithVersion('1.1.4', "copyEvents() with data", function() {
 // should work since 1.1.x when data was introduced
 testWithVersion('1.1.4', "copyEvents() with data", function() {
     var clickCount = mouseoverCount = mouseoutCount = dataCount = 0;
-    
+
     // Create event handlers for A tag
     $('#main a.to')
-        .bind('click', { test: true }, function(event) { 
+        .bind('click', { test: true }, function(event) {
             if (event.data.test) dataCount++;
-            clickCount++; 
+            clickCount++;
         })
         .bind('mouseover', { test: true }, function(event) {
             if (event.data.test) dataCount++;
@@ -131,17 +130,17 @@ testWithVersion('1.1.4', "copyEvents() with data", function() {
             if (event.data.test) dataCount++;
             mouseoutCount++;
         });
-        
+
     // clone events from a to 'from' li tags
     $('#main a.to')
         .copyEventsTo('#main li.to');
-        
+
     $('#main li.to')
         // fire events
         .trigger('click')
         .trigger('mouseover')
         .trigger('mouseout');
-    
+
     // check counts
     ok( clickCount == 2, 'Testing click count' );
     ok( mouseoverCount == 2, 'Testing mouseover count' );
@@ -152,12 +151,12 @@ testWithVersion('1.1.4', "copyEvents() with data", function() {
 // should work since 1.2.x when namespaced events were introduced
 testWithVersion('1.2.6', "copyEvents() with namespaces", function() {
     var clickCount = mouseoverCount = mouseoutCount = dataCount = 0;
-    
+
     // Create event handlers for A tag
     $('#main a.from')
-        .bind('click.yay.query', { test: true }, function(event) { 
+        .bind('click.yay.query', { test: true }, function(event) {
             if (event.data.test) dataCount++;
-            clickCount++; 
+            clickCount++;
         })
         .bind('mouseover.yay.query', { test: true }, function(event) {
             if (event.data.test) dataCount++;
@@ -167,7 +166,7 @@ testWithVersion('1.2.6', "copyEvents() with namespaces", function() {
             if (event.data.test) dataCount++;
             mouseoutCount++;
         });
-        
+
     // clone events from a to 'from' li tags
     $('#main li.from')
         .copyEvents('#main a.from')
@@ -175,7 +174,7 @@ testWithVersion('1.2.6', "copyEvents() with namespaces", function() {
         .trigger('click.yay.query')
         .trigger('mouseover.yay.query')
         .trigger('mouseout.yay.query');
-    
+
     // check counts
     ok( clickCount == 2, 'Testing click count' );
     ok( mouseoverCount == 2, 'Testing mouseover count' );
@@ -186,12 +185,12 @@ testWithVersion('1.2.6', "copyEvents() with namespaces", function() {
 // should work since 1.2.x when namespaced events were introduced
 testWithVersion('1.2.6', "copyEvents() with namespaces", function() {
     var clickCount = mouseoverCount = mouseoutCount = dataCount = 0;
-    
+
     // Create event handlers for A tag
     $('#main a.to')
-        .bind('click.yay.query', { test: true }, function(event) { 
+        .bind('click.yay.query', { test: true }, function(event) {
             if (event.data.test) dataCount++;
-            clickCount++; 
+            clickCount++;
         })
         .bind('mouseover.yay.query', { test: true }, function(event) {
             if (event.data.test) dataCount++;
@@ -201,17 +200,17 @@ testWithVersion('1.2.6', "copyEvents() with namespaces", function() {
             if (event.data.test) dataCount++;
             mouseoutCount++;
         });
-        
+
     // clone events from a to 'from' li tags
     $('#main a.to')
         .copyEventsTo('#main li.to');
-        
+
     $('#main li.to')
         // fire events
         .trigger('click.yay.query')
         .trigger('mouseover.yay.query')
         .trigger('mouseout.yay.query');
-    
+
     // check counts
     ok( clickCount == 2, 'Testing click count' );
     ok( mouseoverCount == 2, 'Testing mouseover count' );
