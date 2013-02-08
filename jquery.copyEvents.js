a99fa3512de8b4256636ecb570e1b22b116a6870
Brandon Aaron <brandon.aaron@gmail.com>
2013-02-08 13:29:44 -0600

    AMD modularized

--- jquery.copyEvents.js
+++ jquery.copyEvents.js
@@ -3,42 +3,47 @@
  *
  * Version: 1.3
  */
-
-(function($, undefined) {
-
-$.fn.extend({
-    copyEvents: function(from) {
-        $.event.copy($(from), this);
-        return this;
-    },
-    
-    copyEventsTo: function(to) {
-        $.event.copy(this, $(to));
-        return this;
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
     }
-});
+}(function ($) {
+   $.fn.extend({
+        copyEvents: function(from) {
+            $.event.copy($(from), this);
+            return this;
+        },
 
-$.event.copy = function(from, to) {
-    // $._data: 1.8 branch
-    // $.data wasn't introduced until the 1.2 branch
-    // 1.1 branch stored event data on the element using "$events" expando
-    // 1.0 branch stored event data on the element using "events" expando
-    var fromEl = from[0],
-        events = fromEl && (($._data && $._data(fromEl, 'events')) || ($.data && $.data(fromEl, 'events')) || fromEl.$events || fromEl.events) || {};
-
-    to.each(function() {
-        var elem = this;
-        for (var type in events) {
-            // in jQuery 1.4.2+ event handlers are stored in an array, previous versions it is an object
-            $.each(events[type], function(index, handler) {
-                // in jQuery 1.4.2+ handler is an object with a handle property
-                // in previous versions it is the actual handler with the properties tacked on
-                var namespaces = handler.namespace !== undefined && handler.namespace || handler.type || '';
-                namespaces = namespaces.length ? (namespaces.indexOf('.') === 0 ? '' : '.')+namespaces : '';
-                $.event.add(elem, type + namespaces, handler.handler || handler, handler.data);
-            });
+        copyEventsTo: function(to) {
+            $.event.copy(this, $(to));
+            return this;
         }
     });
-};
- 
-})(jQuery);
+
+    $.event.copy = function(from, to) {
+        // $._data: 1.8 branch
+        // $.data wasn't introduced until the 1.2 branch
+        // 1.1 branch stored event data on the element using "$events" expando
+        // 1.0 branch stored event data on the element using "events" expando
+        var fromEl = from[0],
+            events = fromEl && (($._data && $._data(fromEl, 'events')) || ($.data && $.data(fromEl, 'events')) || fromEl.$events || fromEl.events) || {};
+
+        to.each(function() {
+            var elem = this;
+            for (var type in events) {
+                // in jQuery 1.4.2+ event handlers are stored in an array, previous versions it is an object
+                $.each(events[type], function(index, handler) {
+                    // in jQuery 1.4.2+ handler is an object with a handle property
+                    // in previous versions it is the actual handler with the properties tacked on
+                    var namespaces = handler.namespace !== undefined && handler.namespace || handler.type || '';
+                    namespaces = namespaces.length ? (namespaces.indexOf('.') === 0 ? '' : '.') + namespaces : '';
+                    $.event.add(elem, type + namespaces, handler.handler || handler, handler.data);
+                });
+            }
+        });
+    };
+}));
